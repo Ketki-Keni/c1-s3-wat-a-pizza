cd3746e70f632a751f772c12884142fe6f049256
Ketki Keni <sketki220@gmail.com>
2022-10-19 21:58:32 +0530

    statement written to display menu to get choice of item from user

--- src/main/java/PizzaOrder.java
+++ src/main/java/PizzaOrder.java
@@ -1,5 +1,6 @@
 public class PizzaOrder {
     public static void main(String[] args) {
+        //Task #1
         String customerName = "Gray" ;
         String customerEmail =  "gary@123.com";
         long phoneNo = 412312345L;
@@ -11,6 +12,12 @@ public class PizzaOrder {
         float garlicBreadPriceIn$ = 5.99f;
         int noOfBeveragesOrdered = 3;
         float beveragePriceIn$ = 1.99f;
-
+        //Task #2
+        System.out.println("Sl No \t Item Category");
+        System.out.println("------------------------");
+        System.out.println("1) \t\t Pizza");
+        System.out.println("2) \t\t Garlic Bread");
+        System.out.println("3) \t\t Beverages");
+        System.out.println("Enter 1 for Pizza, 2 for Garlic Bread and 3 for Beverages");
     }
 }
