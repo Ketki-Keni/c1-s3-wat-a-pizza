a16f88551bd5fa1284e6add9fdbdfeb0a32e919b
Ketki Keni <sketki220@gmail.com>
2022-10-19 21:56:03 +0530

    declared and initialized variables

--- src/main/java/PizzaOrder.java
+++ src/main/java/PizzaOrder.java
@@ -1,5 +1,16 @@
 public class PizzaOrder {
     public static void main(String[] args) {
+        String customerName = "Gray" ;
+        String customerEmail =  "gary@123.com";
+        long phoneNo = 412312345L;
+        String address = "20, Marble Drive, Eville";
+        int noOfPizzasOrdered = 5;
+        float pizzaPriceIn$ = 12.5f;
+        char pizzaSize = 'M';
+        int noOfGarlicBreadOrdered = 3;
+        float garlicBreadPriceIn$ = 5.99f;
+        int noOfBeveragesOrdered = 3;
+        float beveragePriceIn$ = 1.99f;
 
     }
 }
