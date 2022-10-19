fffcd5515a2a3a408302638d6d620c7b1f69791e
Ketki Keni <sketki220@gmail.com>
2022-10-19 22:38:13 +0530

    code written to accept values from user and display the total bill

--- src/main/java/PizzaOrder.java
+++ src/main/java/PizzaOrder.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class PizzaOrder {
     public static void main(String[] args) {
         //Task #1
@@ -5,12 +7,9 @@ public class PizzaOrder {
         String customerEmail =  "gary@123.com";
         long phoneNo = 412312345L;
         String address = "20, Marble Drive, Eville";
-        int noOfPizzasOrdered = 5;
-        float pizzaPriceIn$ = 12.5f;
+        float pizzaPriceIn$ = 12.99f;
         char pizzaSize = 'M';
-        int noOfGarlicBreadOrdered = 3;
         float garlicBreadPriceIn$ = 5.99f;
-        int noOfBeveragesOrdered = 3;
         float beveragePriceIn$ = 1.99f;
         //Task #2
         System.out.println("Sl No \t Item Category");
@@ -18,6 +17,36 @@ public class PizzaOrder {
         System.out.println("1) \t\t Pizza");
         System.out.println("2) \t\t Garlic Bread");
         System.out.println("3) \t\t Beverages");
-        System.out.println("Enter 1 for Pizza, 2 for Garlic Bread and 3 for Beverages");
+        System.out.print("Enter 1 for Pizza, 2 for Garlic Bread and 3 for Beverages : ");
+        Scanner scanner = new Scanner(System.in);
+        int menuItemSlNo = scanner.nextInt();
+        System.out.println();
+        //Task #3
+        switch(menuItemSlNo) {
+            case(1):
+                System.out.print("Please enter the number of pizza you want to order : ");
+                int noOfPizzasOrdered = scanner.nextInt();
+                float totalPriceOfPizzasOrdered = pizzaPriceIn$ * noOfPizzasOrdered;
+                System.out.println();
+                System.out.println("Your total Bill : $" + totalPriceOfPizzasOrdered);
+            break;
+            case(2):
+                System.out.print("Please enter the number of Garlic Bread you want to order : ");
+                int noOfGarlicBreadOrdered = scanner.nextInt();
+                float totalPriceOfGarlicBreadOrdered = garlicBreadPriceIn$ * noOfGarlicBreadOrdered;
+                System.out.println();
+                System.out.println("Your total Bill : $" + totalPriceOfGarlicBreadOrdered);
+            break;
+            case(3):
+                System.out.print("Please enter the number of Beverages you want to order : ");
+                int noOfBeveragesOrdered = scanner.nextInt();
+                float totalPriceOfBeveragesOrdered = beveragePriceIn$ * noOfBeveragesOrdered;
+                System.out.println();
+                System.out.println("Your total Bill : $" + totalPriceOfBeveragesOrdered);
+            break;
+            default:
+                System.out.println("Invalid Sl No selected for menu");
+        }
+
     }
 }
